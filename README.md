0cd7d326687623ed743aab6becf4bec25e8b2930
Charlie Harrington <charlie@whatrocks.org>
2017-10-22 21:02:04 -0700

    ex 1 - 15

---
+++ README.md
@@ -0,0 +1,1 @@
+exercises from LearnPython3TheHardWay
---
+++ ex7.py
@@ -0,0 +1,12 @@
+print("MAry had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+print("." * 10) # what did that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "r"
+
+print(end1 + end2, end=' ')
+print(end3 + end4)
